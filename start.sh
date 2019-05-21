e6d1e379fc83b910b3172946fc3b0ce44e4f58ac
Ilia Lebedev <ilebedev@twitter.com>
2019-05-20 18:10:03 -0700

    Update control scripts

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1
+
 if [ -e ./mflwatcher.pid ]
 then
   echo "ALREADY STARTED. PID: "$(cat ./mflwatcher.pid)
--- status.sh
+++ status.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1
+
 if [ -e ./mflwatcher.pid ]
 then
   PID=$(cat ./mflwatcher.pid)
